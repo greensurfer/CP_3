67388398831bd3e2b18e1e54262b8d0823206913
JasonMcSwagMaster <jason@hiddenme.com>
2019-03-07 10:29:10 -0700

    minor changes only

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let app = new Vue({
   el: '#app',
   data: {
+    current: {
+      question: '',
+      correct_answer: '',
+      incorrect_answers: []
+    },
 
   },
   computed: {
@@ -11,9 +16,23 @@ let app = new Vue({
 
   },
   created() {
-
+    this.trivia();
   },
   methods: {
+    async trivia() {
+      try {
+        this.loading = true;
+        const response = await axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple');
+        this.current = response.data;
+        console.log(this.current);
+        this.loading = false;
+        this.number = response.data.num;
+      } catch (error) {
+        this.loading = false;
+        this.number = this.max;
+        console.log(error);
+      }
+    },
 
   }
 });
